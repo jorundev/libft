72ecb76a8fb0a00a7918fb07a8f8c56d6ffa8418
underdisk <ajendu003@gmail.com>
2020-12-09 12:09:02 +0100

    bref.

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,28 +6,28 @@
 /*   By: hroussea <hroussea@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/23 14:40:10 by hroussea          #+#    #+#             */
-/*   Updated: 2020/12/03 20:56:40 by hroussea         ###   ########lyon.fr   */
+/*   Updated: 2020/12/09 12:08:30 by hroussea         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+char	*ft_strnstr(const char *haystack, const char *needle, size_t size)
 {
 	size_t i;
-	size_t slen;
-
-	slen = ft_strlen(needle);
-	if (slen == 0)
-		return ((char*)haystack);
-	if (len == 0)
-		return (0);
+	size_t j;
 	i = 0;
-	while (i < --len)
+	j = 0;
+	if (!needle[0])
+		return ((char *)haystack);
+	while (haystack[i])
 	{
-		if (ft_strncmp(haystack + i, needle, slen) == 0)
-			return ((char*)haystack + i);
-		++i;
+		while (haystack[i + j] && haystack[i + j] == needle[j] && i + j < size)
+			j++;
+		if (needle[j] == 0)
+			return ((char *)&haystack[i]);
+		i++;
+		j = 0;
 	}
 	return (0);
 }
